e32c424b8114d126192b133217f3587c13814326
Roman Plessl <roman.plessl@oetiker.ch>
2014-02-18 14:54:50 +0100

    added database handling helper scripts

---
+++ src/plugins/pop3-uidl-proxy/database-handling/createDB.sql
@@ -0,0 +1,6 @@
+CREATE TABLE mapping (
+   uidl_seq integer PRIMARY KEY NOT NULL,
+   username TEXT,
+   cuidl    TEXT,
+   zuidl    TEXT
+);
---
+++ src/plugins/pop3-uidl-proxy/database-handling/fillDB.sql
@@ -0,0 +1,11 @@
+INSERT INTO mapping VALUES (1, 'rplessl', '123456789', 'A0123456789');
+INSERT INTO mapping VALUES (2, 'rplessl', '0000000152ef71fa', 'Z000000152ef71fa');
+INSERT INTO mapping VALUES (3, 'rplessl', '0000000252ef71fa', 'Z000000252ef71fa');
+INSERT INTO mapping VALUES (4, 'rplessl', '0000000352ef71fa', 'Z000000352ef71fa');
+INSERT INTO mapping VALUES (5, 'rplessl', '0000000452ef71fa', 'Z000000452ef71fa');
+INSERT INTO mapping VALUES (6, 'rplessl', 'cafeaffe0001',     '262.AdilAtzDMypFIP1Snde,Q2u1aMkA2yjjZVCstCY5Fbc=');
+INSERT INTO mapping VALUES (7, 'rplessl', 'cafeaffe0002',     '265.xJVUPnNApJfmeaA9Uve7QvWRq9T7qu,YtM7UIiQ+BQc=');
+INSERT INTO mapping VALUES (8, 'rplessl', 'cafeaffe0003',     '268.F918nZQCGR5YfvBOF+Mas9n0HB8TLaFRyw6NMah2JAM=');
+INSERT INTO mapping VALUES (9, 'rplessl', 'cafeaffe0004',     '271.dhVIGigvtMx97JDjToEfTgU7HTxiAiHZacw1G5emOg4=');
+
+
